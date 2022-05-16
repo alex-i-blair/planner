64d245c6e05ffd7da288af7d2825457f7a501907
Alex Blair <bclan11@gmail.com>
2022-05-16 15:34:53 -0700

    added context. tests passing

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,28 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Planner from './views/Planner';
 import Header from './components/Header/Header';
 import Entry from './views/Entry';
-
 import './App.css';
+import { PlannerProvider } from './context/PlannerContext';
 
 export default function App() {
   return (
     <> {/* TODO: Add PlannerProvider */}
-      <Header />
-      <BrowserRouter>
-        <Switch>
-          <Route path="/entries/:id">
-            <Entry />
-          </Route>
-          <Route path="/entries">
-            <Planner />
-          </Route>
-          <Route path="/">
-            <Redirect to="/entries" />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <PlannerProvider>
+        <Header />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/entries/:id">
+              <Entry />
+            </Route>
+            <Route path="/entries">
+              <Planner />
+            </Route>
+            <Route path="/">
+              <Redirect to="/entries" />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </PlannerProvider>
     </>
   );
 }
